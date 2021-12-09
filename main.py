73c9951b7c560ff4cfa77aad887b0602731bfd34
do06ami <95838842+do06ami@users.noreply.github.com>
2021-12-09 16:37:29 +0900

    横堀のです

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from diaries.DiarySample import DiarySample
 from diaries.FujisawaDiary import FujisawaDiary
+from diaries.YokoboriDiary import YokoboriDiary
+
 # ↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
     DiarySample(), 
     FujisawaDiary(),
-    ]
+    YokoboriDiary(),
+]
+
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
