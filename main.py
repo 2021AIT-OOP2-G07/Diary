fac99783bd447b4b13300d8edbd5bee857d3aba4
fujisawajoe <95838761+fujisawajoe@users.noreply.github.com>
2021-12-16 13:30:17 +0900

    mainが動かないので多少改良2🍣
    diariesを付加

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 from diaries.DiarySample import DiarySample
-<<<<<<< Updated upstream
-=======
 from diaries.TakagiDiary import TakagiDiary
-from diaries.KamiyaDiary import KamiyaDiary
->>>>>>> Stashed changes
+from diaries.UkyoDiary import UkyoDiary
 from diaries.FujisawaDiary import FujisawaDiary
 # ↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
     DiarySample(), 
     FujisawaDiary(),
-<<<<<<< Updated upstream
-    ]
-=======
-    YokoboriDiary(),
-    KamiyaDiary(),
-     a_imaiDiary(),
-     UkyoDiary(),
+    UkyoDiary(),
     TakagiDiary(),
 ]
 
->>>>>>> Stashed changes
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
