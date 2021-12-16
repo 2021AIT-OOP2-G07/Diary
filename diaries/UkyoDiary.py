f8fa3122d143d4bc9aa34bcdde0eb1e4002c82c2
fujisawajoe <95838761+fujisawajoe@users.noreply.github.com>
2021-12-16 13:20:57 +0900

    mainが動かないので多少改良🍣
    AbstractDiaryにdiariesを付加
    慣例に従ってtakagiDiaryをTakagiDiaryに名称変更

---
+++ diaries/UkyoDiary.py
@@ -0,0 +1,13 @@
+from diaries.AbstractDiary import AbstractDiary
+
+
+class UkyoDiary(AbstractDiary):
+
+    def get_date(self):
+        return "2021-12-09"
+
+    def get_summary(self):
+        return """今日は本格的にチームでの作業をした。訳が分からなかった。なんとか頑張る"""
+
+    def get_author(self):
+        return "Ukyo"
--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from diaries.DiarySample import DiarySample
+<<<<<<< Updated upstream
+=======
+from diaries.TakagiDiary import TakagiDiary
+from diaries.KamiyaDiary import KamiyaDiary
+>>>>>>> Stashed changes
 from diaries.FujisawaDiary import FujisawaDiary
 # ↓のリストには、メンバーの各⽇記が格納されます。
 diaries = [
     DiarySample(), 
     FujisawaDiary(),
+<<<<<<< Updated upstream
     ]
+=======
+    YokoboriDiary(),
+    KamiyaDiary(),
+     a_imaiDiary(),
+     UkyoDiary(),
+    TakagiDiary(),
+]
+
+>>>>>>> Stashed changes
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
